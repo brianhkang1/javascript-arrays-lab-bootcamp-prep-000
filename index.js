b9d4f4fd57af8acd1dd07d3af63393e200df7022
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-04 19:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
 
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function 
